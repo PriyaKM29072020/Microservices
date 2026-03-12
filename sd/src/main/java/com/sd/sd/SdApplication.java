08be63a84c9cc72937eb93c736ea9a5f144e1bdd
PriyaKM29072020 <priyankakande1993@gmail.com>
2026-03-12 21:24:54 +0530

    sd code

--- sd/src/main/java/com/sd/sd/SdApplication.java
+++ sd/src/main/java/com/sd/sd/SdApplication.java
@@ -2,8 +2,10 @@ package com.sd.sd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SdApplication {
 
 	public static void main(String[] args) {
