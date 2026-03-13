6f059242e02c204bd50884c5a5c3376129a9a16e
PriyaKM29072020 <priyankakande1993@gmail.com>
2026-03-13 15:52:09 +0530

    add security

--- consumer/src/main/java/com/consumer/consumer/openfeign/ProducerController.java
+++ consumer/src/main/java/com/consumer/consumer/openfeign/ProducerController.java
@@ -3,7 +3,7 @@ package com.consumer.consumer.openfeign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "producer-service", url = "http://localhost:9090/producer")
+@FeignClient(name = "producer-service", url = "http://localhost:9090")
 public interface ProducerController {
     @GetMapping("hello")
     String hello();
